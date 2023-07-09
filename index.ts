43ceae4a45881a6cb263fb7ce73590322ddaa9d3
adhiramaa <vinitfagna@gmail.com>
2023-07-10 00:36:59 +0530

    Implement type annotations

--- index.ts
+++ index.ts
@@ -1,3 +1,25 @@
+import * as _ from 'lodash';
 async function hello() {
     return 'world'
+}
+/*Type annotations code from now
+ any means compiler want type check it
+ let lucky: any = 23;
+ If you have a implicit type don't bother explicitly strong gtype in it
+*/
+
+//My own type:- Style is a union by writing the pipe symbole to separate 
+type Style = 'bold' | 'italic' | 23;
+//annoting Style type >
+let font: Style;
+
+//Creating object 
+const person = {
+    firstName: 'John',
+    lastName: 'XXX'
+}
+
+const person2 = {
+    firstName: 'ksks',
+    lastName: 'X7fX'
 }
