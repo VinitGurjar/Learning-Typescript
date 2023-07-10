a81bbca802ed8c978eb5525d9fc2e0f2c6e45f08
adhiramaa <vinitfagna@gmail.com>
2023-07-10 18:17:42 +0530

    Implement function types

--- index.ts
+++ index.ts
@@ -29,3 +29,12 @@ const person2: Person = {
     firstName: 'ksks',
     lastName: 'X7fX'
 }
+
+//Functions
+//string is the return type of the function
+
+function pow(x: number, y: number): string {
+    return Math.pow(x, y).toString();
+}
+
+pow(5, 10)
