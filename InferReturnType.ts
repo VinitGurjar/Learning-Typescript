c890375bae313ef14f853f1c9bea5d02c0e3ee78
vinit gurjar <vinitfagna@gmail.com>
2023-07-15 18:22:22 +0000

    add: Explicit return type

--- InferReturnType.ts
+++ InferReturnType.ts
@@ -16,3 +16,29 @@ function getRandomNumber(){
 
   
   let myVar = getRandomNumber();
+
+ // Explicit Return Types
+/*If we’d like to be explicit about what type a function returns,
+ we can add an explicit type annotation after its closing parenthesis
+ . Here, we use the same syntax as other type annotations, a colon 
+ followed by the type. TypeScript will produce an error for any return
+  statement in that function that doesn’t return the right type of value.*/
+
+function createGreeting(name?: string): string {
+  if (name) {
+    return `Hello, ${name}!`;
+  }
+ 
+  return undefined;
+  // Typescript Error: Type 'undefined' is not assignable to type 'string'.
+};
+
+//arrow function
+const createArrowGreeting = (name?: string): string => {
+  if (name) {
+    return `Hello, ${name}!`;
+  }
+ 
+  return undefined;
+  // Typescript Error: Type 'undefined' is not assignable to type 'string'.
+};
